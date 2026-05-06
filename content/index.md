356cca0007805c456e31c3002a242e10642fb488
YoullFloatToo <giuseppe.caringella00@gmail.com>
2026-05-05 19:18:51 -0500

    Quartz sync: May 5, 2026, 7:18 PM

--- content/index.md
+++ content/index.md
@@ -1,6 +1,14 @@
 ---
-title: Welcome to Quartz
+title: Unwelcome to Althrea
 ---
+Althrea does not invite you in.
 
-This is a blank Quartz installation.
-See the [documentation](https://quartz.jzhao.xyz) for how to get started.
+Here, you will not find a soft bed for landing. No comfortable confines in which to play, colored with the light of the golden sun and bursting lush with life. It's a world of brutal hostility and death.
+
+However, that doesn't mean Althrea is completely inhospitable. It has its healthy share of magic and whimsy, like its particular [[Humor in Althrea|flavor]] of humor and [[Snapper Frog|exploding frogs]]. The people here *do* find a way to live, and even thrive in this world. Yet, in doing so, they bask in a dangerous air of acceptance, ignorant to the workings at their feet.
+
+Buried somewhere deep inside, a cold horror exists. Weaving itself in between the cracks in the earth, wrapping itself around roots and coiling through sands. And sometimes, influencing the minds of men.
+
+Unwelcome to Althrea.
+
+*─ The Narrator*
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Althrea",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -23,10 +23,17 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Oxanium",
+        body: "Oxanium",
+        code: "Oxanium",
       },
+
+/**
+ *	header: "Schibsted Grotesk",
+ *	body: "Source Sans Pro",
+ *	code: "IBM Plex Mono",
+*/
+
       colors: {
         lightMode: {
           light: "#faf8f8",
@@ -34,7 +41,7 @@ const config: QuartzConfig = {
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
+          secondary: "#8770c4",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
@@ -45,7 +52,7 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
+          secondary: "#8770c4",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
@@ -90,6 +97,7 @@ const config: QuartzConfig = {
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
+	Plugin.Static(),
     ],
   },
 }
